960251c290e3c371c540b908123bbc2789b88f24
Khaled <khaled.j@hungerstation.com>
2020-12-30 02:58:57 +0300

    Auth

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = ''
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-7-0pa3gh.us.auth0.com',            // Auth0 domain
+  clientId: 'BBbtRVr6tynpbFh9q4hd6kW2lFK2fQhz',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
